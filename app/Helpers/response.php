185ef3701f7c55cf8ec1158a1bbde3856c3534b7
Gabriel David <engine@play-zone.ro>
2023-08-05 01:54:57 +0300

    helpers: response

---
+++ app/Helpers/response.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Response;
+
+if (!function_exists('isValidHttpResponseCode')) {
+    function isValidHttpResponseCode(int $code): bool
+    {
+        return array_key_exists($code, Response::$statusTexts);
+    }
+}
+
+if (!function_exists('returnValidHttpResponseCode')) {
+    function returnValidHttpResponseCode(int $providedCode, int $fallbackCode): int
+    {
+        return isValidHttpResponseCode($providedCode) ? $providedCode : $fallbackCode;
+    }
+}
