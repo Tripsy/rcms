f5307e9a44e753c8cda56e4d9ca9379b39f09e5c
Tripsy <Tripsy@users.noreply.github.com>
2024-02-11 12:23:12 +0000

    Apply Pint / PHP-CS-Fixer changes

--- app/Helpers/response.php
+++ app/Helpers/response.php
@@ -2,14 +2,14 @@
 
 use Symfony\Component\HttpFoundation\Response;
 
-if (! function_exists('isValidHttpResponseCode')) {
+if ( ! function_exists('isValidHttpResponseCode')) {
     function isValidHttpResponseCode(int $code): bool
     {
         return array_key_exists($code, Response::$statusTexts);
     }
 }
 
-if (! function_exists('returnValidHttpResponseCode')) {
+if ( ! function_exists('returnValidHttpResponseCode')) {
     function returnValidHttpResponseCode(int $providedCode, int $fallbackCode): int
     {
         return isValidHttpResponseCode($providedCode) ? $providedCode : $fallbackCode;
